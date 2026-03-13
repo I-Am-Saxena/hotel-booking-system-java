8aaaadb602e9cd99bd191b6424db8d70a5058e9f
101-Knight <sanvigoel2005@gmail.com>
2026-03-13 14:21:02 +0530

    UC1: Application entry point and welcome message

---
+++ src/main/java/com.hotel.booking/app/HotelBookingApplication.java
@@ -0,0 +1,22 @@
+package com.hotel.booking.app;
+
+/**
+ * Hotel Booking Application Entry Point
+ *
+ * This class represents the starting point of the application.
+ * The JVM invokes the main() method to start execution.
+ *
+ * @author Lakshmi
+ * @version 1.0
+ */
+public class HotelBookingApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Application Version : v1.0 ");
+        System.out.println("=================================");
+
+    }
+}
